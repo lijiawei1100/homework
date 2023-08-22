40e254488db566bb3f7bbbeedd5dce165fe86da0
Lenovo <u7531534@anu.edu.au>
2023-08-22 11:12:06 +1000

    D2B from Jiawei Li

---
+++ src/comp1110/ass2/Assam.java
@@ -0,0 +1,23 @@
+package comp1110.ass2;
+
+public class Assam {
+    /**initialize the statuses of Assam,including direction,position
+    * we have four direction(up,right,down,left),but we can use angle(0,90,180.270)
+     * for the position, we need to initialize a square Intpair[] for it*/
+    int direction;
+    String current Assam;
+
+
+
+    /**to return Assam's direction */
+    getAssamDirection(){
+         return direction;
+    }
+
+    /**to return Assam's square Intpair[] position*/
+    getAssamPositon(){
+    }
+
+
+
+}
